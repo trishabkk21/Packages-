94a8b3a66acd519f058ddd026e81fddd8fab40b8
WillMcBurney <pmcburn1@gmail.com>
2022-09-05 13:57:23 -0400

    Added a test, but still need to figure out how to mock file output.

---
+++ src/test/java/edu/virginia/cs/nbateams/NBATeamExcelWriterTest.java
@@ -0,0 +1,13 @@
+package edu.virginia.cs.nbateams;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NBATeamExcelWriterTest {
+    @Test
+    public void testWrite() {
+
+    }
+
+}
